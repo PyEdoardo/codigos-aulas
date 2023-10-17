6dd19720d2e2f628962b49fbbf00db6c6e0d39bc
Edoardo Tombolesi <42304654+SrDuduBR@users.noreply.github.com>
2023-10-17 16:34:57 -0700

    Add files via upload

---
+++ 1-AT-Listas.py
@@ -0,0 +1,69 @@
+#listaaluno = ["Bruno", "Ana", "Bia", "Eva"] #Lista Básica de Strings em Python
+#print(listaaluno) #Print dos elementos da lista
+
+#num = [1,2,3,4,5] #Lista Básica de Inteiros
+#print(num) #Print dos Inteiros
+
+#lista_aluno = ["Ana", 8.4, "Bia", 10, "Eva", 5.5, "Edoardo", 10.50] #Lista com valores Strings e Float
+#print("Tamanho da Lista: ", len(lista_aluno)) #Print usando função len, para mostrar a quantidade de elementos da lista
+#cont = len(lista_aluno) #Flag do While usando os elementos da lista
+#print(cont)
+
+#nome = input("Nome: ")
+#nomes = ["Ana", "Victor", "Bia"]
+
+#if nome in nomes: #If básico para saber se existe a string da variável nome, dentro da lista nomes
+    #print("Existe na Lista")
+#else:
+    #print("Não Existe! ")
+
+
+
+#nomes = ["Ana", "Victor", "Bia"] #Usando [] para mostrar apenas alguns valores da Lista
+#print("Nome: ", nomes[0]) #o [] com número 0 mostra apenas o nome Ana
+#print("Nome: ", nomes[1])
+#print("Nome: ", nomes[2])
+
+#nomes = ["Ana", "Victor", "Bia"]
+#cont = 0
+#while cont < len(nomes): #Usando While para verificar cada nome por vez
+#    print("Posição: ", cont, "Nome: ", nomes[cont]) #Usando a Flag para olhar algum elemento específico da lista
+#    cont = cont + 1 #Para não ficar em loop
+
+
+#nomes = []
+#print("Tamanho: ", len(nomes), nomes)
+#nomes.append("Ana")
+#print("Tamanho: ", len(nomes), nomes)
+#nomes.append("Bia")
+#print("Tamanho: ", len(nomes), nomes)
+#nomes.append("Edoardo")
+#print("Tamanho: ", len(nomes), nomes)
+
+#lista_nomes = ["Ana", "Bia", "Eva"]
+#print(lista_nomes)
+#lista_nomes[1] = "Sara"
+#print(lista_nomes)
+
+
+#nomes = []
+#cont = 0
+#while cont < 4:
+#    nomes.append(input("Digite o Nome: "))
+#    print("Tamanho da Lista: ", len(nomes), nomes)
+#    cont = cont + 1
+
+
+
+#numeros = []
+
+#flag = 0
+#while flag != 0:
+#    var_num = int(input("Digite o Número Inteiro: "))
+#    numeros.append(var_num)
+#    flag = flag + 1
+#print("Elementos: ", )
+#flag = 0
+#while flag < len(numeros):
+#    print(numeros[flag])
+#    flag = flag + 1
