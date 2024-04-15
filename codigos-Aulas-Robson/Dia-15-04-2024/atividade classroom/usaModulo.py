659fb5354d342e4980b02901f8b365bd8973eff6
Edoardo <edoardotombolesi@rede.ulbra.br>
2024-04-15 19:34:22 -0300

    sla

---
+++ codigos-Aulas-Robson/Dia-15-04-2024/atividade classroom/usaModulo.py
@@ -0,0 +1,32 @@
+import modulo
+## ATV 1:
+
+print(modulo.interrogatorio())
+
+## ATV 2:
+
+notas = []
+
+print("Soma dos valores:", modulo.soma)
+print("Média dos valores:", modulo.media)
+print("Quantidade de valores acima da média:", modulo.acima_media)
+print("Quantidade de valores abaixo de sete:", modulo.abaixo_sete)
+print("Programa encerrado.")
+
+## ATV 3:
+
+modulo.calcula_salarios()
+
+## ATV 4:
+
+modulo.competicao_salto()
+
+## ATV 5:
+
+votos = int(input("Digite os votos: "))
+total_votos = int(input("Total de votos: "))
+print(modulo.calcula_percentual(votos, total_votos))
+
+## ATV 6:
+
+modulo.enquete()
