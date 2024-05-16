6d29741dfd261a85574f8ccdaf6d83d377f8bc89
edoardo <edoardotombolesi@rede.ulbra.br>
2024-05-15 21:28:28 -0300

    sla

--- codigos-Aulas-Robson/Dia-13-05-2024/atividade-1/funcs.py
+++ codigos-Aulas-Robson/Dia-13-05-2024/atividade-1/funcs.py
@@ -4,3 +4,19 @@ def procLogin(login, usuarios):
         return 'Não existe este usuário!'
     else:
         return usuarios[login]
+def procData(data, usuarios):
+    dictData = {}
+    for key in usuarios:
+        if data not in usuarios[key]['ultimo_acesso']:
+            return 'Não existe essa data!'
+        elif data == usuarios[key]['ultimo_acesso']:
+            dictData[key] = {'nome' : usuarios[key]['nome'], 'ultimo_acesso' : usuarios[key]['ultimo_acesso']}
+    return dictData
+
+def modificarNome(login, novoNome, usuarios):
+    if login not in usuarios:
+        return 'Não existe esse usuário!'
+    else:
+        usuarios[login]['nome'] == novoNome
+        
+          
--- codigos-Aulas-Robson/Dia-13-05-2024/atividade-1/programa.py
+++ codigos-Aulas-Robson/Dia-13-05-2024/atividade-1/programa.py
@@ -2,7 +2,7 @@ import funcs
 
 usuarios = {
  'edo' : {'nome' : 'Edoardo Tombolesi', 'ultimo_acesso' : '24/10/2023', 'id_maquina' : 'labin12'},
- 'gab' : {'nome' : 'Gabriel', 'ultimo_acesso' : '23/15/2027', 'id_maquina' : 'labin34'}
+ 'gab' : {'nome' : 'Gabriel', 'ultimo_acesso' : '24/10/2023', 'id_maquina' : 'labin34'}
 }
 
 while True:
@@ -16,13 +16,25 @@ while True:
         print("Login já cadastrado!")
 
 while True:
-    opcao = int(input("Qual opção deseja? \n(1: Listar todos os nomes de usuários\n2: Listar dados de um usuário\n3: Listar os nomes de usuários de uma data específica\n4: Alterar dados de um usuário\n5: Excluir um usuário\n6: Sair) "))
+    opcao = int(input("Qual opção deseja? \n1: Listar todos os nomes de usuários\n2: Listar dados de um usuário\n3: Listar os nomes de usuários de uma data específica\n4: Alterar dados de um usuário\n5: Excluir um usuário\n6: Sair) "))
     
     if opcao == 6:
         break
-    if opcao == 1:
+    elif opcao == 1:
         for chave in usuarios:
             print(f'Nome: {usuarios[chave]["nome"]}')
-    if opcao == 2: 
+    elif opcao == 2: 
         proc_user = input("Qual o Login a ser procurado: ")
         funcs.usuarios(proc_user, usuarios)
+    elif opcao == 3:
+        dataProc = input("Qual a data a ser pesquisada: ")
+        print(f'{funcs.procData(dataProc, usuarios)}')
+    elif opcao == 4:
+        print("Opções: (todos, nome, último acesso ou id da máquina)\n")
+        oqModificar = input("Qual opção: ")
+        loginModificado = input("Qual o login que pretende modificar: ")
+        if oqModificar == 'nome':
+            novoNome = input("Digite o novo nome: ")
+            print(f'Nome Antigo: {usuarios[loginModificado]["nome"]}, Novo Nome: {funcs.modificarNome(loginModificado, novoNome, usuarios)}')
+
+
