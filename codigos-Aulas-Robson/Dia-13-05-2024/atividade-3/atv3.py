b67bc6d517314c56467eafdccc51a7c371ed10d0
edoardo <edoardotombolesi8@gmail.com>
2024-05-16 02:20:49 -0300

    fácil robson melhore

---
+++ codigos-Aulas-Robson/Dia-13-05-2024/atividade-3/atv3.py
@@ -0,0 +1,21 @@
+palavras = {
+
+}
+
+while True:
+    deseja = input('Deseja inserir palavras? (sim para continuar e não para parar.): ')
+    if deseja == 'não' or deseja == 'nao':
+        break
+    palavraIng = input('Digite a palavra em inglês: ')
+    palavraPT = input('Digite a palavra em portugês: ')
+    palavras[palavraIng] = palavraPT
+
+
+while True:
+    procurarIng = input('Qual palavra em inglês deseja procurar (0 para encerrar): ')
+    if procurarIng == '0':
+        break
+    if procurarIng not in palavras:
+        print("Palavra não encontrada!")
+    else:
+        print(f'Palavra em Inglês: {palavraIng} | Palavra em Português: {palavras.get(procurarIng)}')
