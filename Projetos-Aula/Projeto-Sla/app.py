01cf08043a98334e874e852877e94c2645f2f2b4
Edoardo <edoardotombolesi8@gmail.com>
2024-05-28 21:24:14 -0300

    adição

--- Projetos-Aula/Projeto-Sla/app.py
+++ Projetos-Aula/Projeto-Sla/app.py
@@ -1,8 +1,12 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
+class Incrementador(BoxLayout):
+    pass
 
 class Teste(App):
     def build(self):
-        return Button(text=f'{None}')
-
+        return Incrementador()
 Teste().run()
