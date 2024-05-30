18fe708c370b6c80550bdd4be6a4932d36166e1a
edoardo <edoardotombolesi8@gmail.com>
2024-05-30 20:57:20 -0300

    sla

--- Projetos-Aula/Projeto-Calculadora-Interface/interface.py
+++ Projetos-Aula/Projeto-Calculadora-Interface/interface.py
@@ -1,1 +1,11 @@
+from kivy.app import App
+from kivy.lang import Builder
+import os.path
 
+GUI = Builder.load_file("tela.kv")
+
+class Aplicativo(App):
+    def build(self):
+        return GUI
+
+Aplicativo().run()
--- Projetos-Aula/Projeto-POO/poo.py
+++ Projetos-Aula/Projeto-POO/poo.py
@@ -1,1 +1,14 @@
+import funcs
+
+class Usuario:
+    def __init__(self, email, nomeUser, senha, dataNascimento, sexo):
+        self.email = email
+        self.nomeUser = nomeUser
+        self.senha = senha
+        self.dataNascimento = dataNascimento
+        self.sexo = sexo
+        pass
+    pass
+
+usuarios = {}
 
