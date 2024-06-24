acb66783af3b85dc0fb7c017fc6dcba2f39ced15
edoardo <edoardotombolesi8@gmail.com>
2024-06-24 15:11:04 -0300

    terminei poRRAAAAAAAAAAAAA

--- codigos-Aulas-Robson/Teste-ATV-10-06-2024/funcs.py
+++ codigos-Aulas-Robson/Teste-ATV-10-06-2024/funcs.py
@@ -29,8 +29,20 @@ def situacaoAlunos(arquivojson):
     arquivo.close()
     return situacao
 
-def alterarSituacao(alunos ,matriculaAlterar, nota1Nova, nota2Nova, arquivojson):
-    
+def alterarSituacao(matriculaAlterar, nota1Nova, nota2Nova, frequenciaNova, arquivojson):
+    arquivo = open(arquivojson, 'r', encoding='utf8')
+    dicionario_alunos = json.load(arquivo)
+    arquivo.close()
+
+    if matriculaAlterar in dicionario_alunos:
+        dicionario_alunos[matriculaAlterar]['nota1'] = nota1Nova
+        dicionario_alunos[matriculaAlterar]['nota2'] = nota2Nova
+        dicionario_alunos[matriculaAlterar]['percentualFrequencia'] = frequenciaNova
+    else:
+        return 'Erro'
+    arquivo = open(arquivojson, 'w', encoding='utf8')
+    json.dump(dicionario_alunos, arquivo, indent=4, ensure_ascii=False)
+    arquivo.close()
 
 
 def exportarSituacao(situacao, arquivoTexto):
@@ -38,6 +50,30 @@ def exportarSituacao(situacao, arquivoTexto):
     for aluno in situacao:
         arquivo.write(f'{situacao[aluno]["matricula"]} ,{situacao[aluno]["nome"]} ,{situacao[aluno]["situacao"]}\n')
     arquivo.close()
+
+def adicionarAluno(nome, nota1, nota2, frequencia, arquivojson):
+    arquivo = open(arquivojson, 'r', encoding='utf8')
+    dicionario_Alunos = json.load(arquivo)
+    arquivo.close()
+
+    maior_matrícula = 0
+
+    for matricula in dicionario_Alunos.keys():
+        numero_matricula = int(matricula)
+        if numero_matricula < maior_matrícula:
+            numero_matricula = maior_matrícula
+    numero_matricula = numero_matricula + 1
+
+    dicionario_Alunos[numero_matricula] = {
+        'nome' : nome,
+        'nota1' : nota1,
+        'nota2' : nota2,
+        'percentualFrequencia' : frequencia
+    }
+
+    arquivo = open(arquivojson, 'w', encoding='utf8')
+    json.dump(dicionario_Alunos, arquivo, indent=4, ensure_ascii=False)
+
 
 
 
--- codigos-Aulas-Robson/Teste-ATV-10-06-2024/prova.py
+++ codigos-Aulas-Robson/Teste-ATV-10-06-2024/prova.py
@@ -3,7 +3,7 @@ import funcs
 arquivojson = 'alunos.json'
 arquivotexto = 'situacao.txt'
 while True:
-    print("Opções: Situação Alunos (1), Alterar Notas (2), Exportar Situação (3), Fechar Programa (0)\n")
+    print("Opções: Situação Alunos (1), Alterar Notas (2), Exportar Situação (3), Adicionar Aluno (4), Fechar Programa (0)\n")
     menu = int(input("Qual será a sua opção: "))
     if menu == 0:
         break
@@ -15,8 +15,15 @@ while True:
         matriculaAlterar = input("Qual matrícula gostaria de alterar: ")
         nota1Nova = float(input("Digite a nova nota1: "))
         nota2Nova = float(input("Digite a nova nota2: "))
-        print(funcs.alterarNotas(matriculaAlterar, nota1Nova, nota2Nova, arquivojson))
+        frequenciaNova = float(input("Digite a nova Frequência: "))
+        print(funcs.alterarSituacao(matriculaAlterar, nota1Nova, nota2Nova, frequenciaNova, arquivojson))
     elif menu == 3:
         funcs.exportarSituacao(funcs.situacaoAlunos(arquivojson), arquivotexto)
+    elif menu == 4:
+        nome = input("Qual o nome do Aluno: ")
+        nota1 = float(input("Digite a nota1 do Aluno: "))
+        nota2 = float(input("Digite a nota2 do Aluno: "))
+        frequencia = float(input("Digite a Frequência do Aluno: "))
+        funcs.adicionarAluno(nome, nota1, nota2, frequencia, arquivojson)
         
 
