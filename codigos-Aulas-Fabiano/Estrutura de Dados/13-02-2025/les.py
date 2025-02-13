c97f8d4e78d83128f1baa10b5f14d5e31128ed20
Edoardo <edoardotombolesi8@gmail.com>
2025-02-12 21:46:07 -0300

    termino aula dia 12/02/2025

--- codigos-Aulas-Fabiano/Estrutura de Dados/13-02-2025/les.py
+++ codigos-Aulas-Fabiano/Estrutura de Dados/13-02-2025/les.py
@@ -3,7 +3,22 @@ class Les:
         self.tam = tamanho
         self.quant = 0
         self.vetor = [None] * self.tam
+
     def inserir_fim(self, valor):
         self.vetor[self.quant] = valor
         self.quant += 1
+
+    def show(self):
+        for i in range(self.quant):
+            print(self.vetor[i], end=' ')
+        print("\n")
+    
+    def remover_fim(self):
+        self.quant -= 1
+    
+    def esta_cheia(self):
+        return self.quant == self.tam
+    
+    def esta_vazia(self):
+        return self.quant == 0
     
--- codigos-Aulas-Fabiano/Estrutura de Dados/13-02-2025/testaLes.py
+++ codigos-Aulas-Fabiano/Estrutura de Dados/13-02-2025/testaLes.py
@@ -2,12 +2,21 @@ import les
 
 l = les.Les(5)
 
+print("Antes remover")
 l.inserir_fim("A")
 l.inserir_fim("B")
 l.inserir_fim("C")
 l.inserir_fim("D")
 
-print(l.vetor)
+if not l.esta_cheia():
+    l.inserir_fim("E")
+else:
+    print("Está cheia")
+
+if not l.esta_vazia():
+    l.inserir_fim("F")
+else:
+    print("Está cheio")
 
 
 
