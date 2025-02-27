b720b8792cbbe086768d0237458e2d032e5c0ebe
Edoardo Tombolesi <edoardotombolesi@rede.ulbra.br>
2025-02-26 21:51:36 -0300

    finalização da atividade

--- codigos-Aulas-Fabiano/Estrutura de Dados/26-02-2025/Ldse/Ldse.py
+++ codigos-Aulas-Fabiano/Estrutura de Dados/26-02-2025/Ldse/Ldse.py
@@ -13,4 +13,22 @@ class Ldse:
     def inserir_inicio(self, valor):
         if self.quant == 0:
             self.prim = self.ult = No(valor, None)
-        
+        else:
+            self.prim = No(valor, self.prim)
+        self.quant += 1
+
+    def inserir_final(self, valor):
+        pass
+    
+    def remover_inicio(self):
+        if self.quant == 1:
+            self.prim = self.ult = None
+        else:
+            self.prim = self.prim.prox
+        self.quant -= 1
+
+    def show(self):
+        aux = self.prim
+        for i in range(self.quant):
+            print(aux.info)
+            aux = aux.prox
--- codigos-Aulas-Fabiano/Estrutura de Dados/26-02-2025/Ldse/testeLdse.py
+++ codigos-Aulas-Fabiano/Estrutura de Dados/26-02-2025/Ldse/testeLdse.py
@@ -3,3 +3,6 @@ import Ldse
 l = Ldse.Ldse()
 
 l.inserir_inicio('A')
+l.inserir_inicio('B')
+l.inserir_inicio('C')
+l.show()
