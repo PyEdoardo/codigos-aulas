5c3a833799adc5c6d81dd895085f5514337e3faa
Edoardo Tombolesi <edoardotombolesi@rede.ulbra.br>
2025-04-03 17:34:58 -0300

    adição de LDdec, com erro porém bem feito até..

--- codigos-Aulas-Fabiano/Estrutura de Dados/02-04-2025/Lddec/Lddec.py
+++ codigos-Aulas-Fabiano/Estrutura de Dados/02-04-2025/Lddec/Lddec.py
@@ -6,4 +6,54 @@ class No:
 
 class Lddec:
     def __init__(self):
-        pass
+        self.prim = self.ult = None
+        self.quant = 0
+    
+    def inserir_inicio(self, valor):
+        if self.quant == 0:
+            self.ult = self.prim = No(self.ult, valor, self.prim)
+        else: 
+            self.prim.ant = self.prim = No(self.ult, valor, self.prim)
+            self.ult.prox = self.prim
+        self.quant += 1
+        
+    def inserir_final(self, valor):
+        if self.quant == 0:
+            self.ult = self.prim = No(self.ult, valor, self.prim)
+        else:
+            self.ult.prox = self.ult = No(self.ult, valor, self.prim)
+            self.prim.ant = self.ult
+        self.quant += 1
+        
+        
+    def show(self):
+        aux = self.prim
+        
+        #Teste
+        prox = aux.prox
+        ant = aux.ant
+        for i in range(self.quant):
+            print(f'Valor: {aux.info} | Próximo: {prox.info} | Anterior: {ant.info}')
+            aux = aux.prox
+            prox = aux.prox
+            ant = aux.ant
+            
+    def show_inverso(self):
+        aux = self.ult
+        
+        #Teste
+        prox = aux.prox
+        ant = aux.ant
+        for i in range(self.quant):
+            print(f'Valor: {aux.info} | Próximo: {prox.info} | Anterior: {ant.info}')
+            aux = aux.ant
+            prox = aux.prox
+            ant = aux.ant
+    
+    def inserir_valores_final(self, *valores):
+        if len(valores) <= 1:
+            return
+        for i in valores:
+            self.ult.prox = self.ult = No(self.ult, i, self.prim)
+            self.ult.ant = self.ult
+            self.quant += 1
